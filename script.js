c0bf799074a364ed2bfc005082b5c288458fa4f5
semilore <osunsanmisemilore@gmail.com>
2023-06-13 20:46:29 +0100

    Creating Dashboard

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 const fileInput = document.getElementById('file-input');
 const fileLabel = document.querySelector('#file-chosen');
+const checkIcon = document.querySelector('.fa-check-circle')
 
 fileInput.addEventListener('change', (event) => {
   const fileName = event.target.files[0].name;
   const trimmedFileName = fileName.length > 8 ? fileName.substring(0,8) + '...': fileName
   fileLabel.textContent = trimmedFileName;
+  checkIcon.style.display = "inline";
 });
