dccda628cd315bac0b3c1aa50d3d0d762042d9be
semilore <osunsanmisemilore@gmail.com>
2023-06-14 18:10:16 +0100

    Creating Dashboard

--- script.js
+++ script.js
@@ -11,16 +11,26 @@ fileInput.addEventListener('change', (event) => {
 });
 
 
-
 document.addEventListener("DOMContentLoaded", function() {
   var slideDiv = document.getElementById("slideDiv");
   var scrollThreshold = 80; // Adjust the desired scroll threshold here
   var isDivVisible = true;
-
+  var isScrolling = false;
+  
   // Initially show the slideDiv
   slideDiv.classList.add("show");
 
-  window.addEventListener("scroll", function() {
+  function debounce(func, delay) {
+    var timer;
+    return function() {
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+        func();
+      }, delay);
+    };
+  }
+
+  function handleScroll() {
     var scrollPos = window.scrollY || window.pageYOffset;
 
     if (scrollPos > scrollThreshold && isDivVisible) {
@@ -30,6 +40,19 @@ document.addEventListener("DOMContentLoaded", function() {
       slideDiv.classList.add("show");
       isDivVisible = true;
     }
-  });
-});
+    
+    isScrolling = false;
+  }
 
+  window.addEventListener("scroll", function() {
+    if (!isScrolling) {
+      isScrolling = true;
+      requestAnimationFrame(function() {
+        handleScroll();
+      });
+    }
+  });
+
+  var debouncedHandleScroll = debounce(handleScroll, 200);
+  window.addEventListener("scroll", debouncedHandleScroll);
+});
