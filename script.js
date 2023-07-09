c65f7cf98c92dfd6f8761479eafe8e17a49ba60d
semilore <osunsanmisemilore@gmail.com>
2023-07-09 21:24:06 +0100

    Making final adjustments before backend part 1

--- script.js
+++ script.js
@@ -33,56 +33,6 @@
       });
 
 
-/*Bottom Naviagtion Effects*/
-
-      var homeIcon = document.querySelector("#home-icon");
-      var searchIcon = document.querySelector("#search-icon");
-      var notificationIcon = document.querySelector("#notification-icon");
-      var moreSettingsIcon = document.querySelector("#more-settings-icon");
-      var searchBar = document.querySelector(".search-bar");
-      var notificationPopup = document.querySelector(".notification-popup");
-      var moreSettingsPopup = document.querySelector(".more-settings-popup");
-
-      homeIcon.addEventListener("click", function () {
-        scrollToTop();
-        closeMenus();
-      });
-
-      searchIcon.addEventListener("click", function () {
-        closeMenus();
-        searchBar.style.display = "block";
-      });
-
-      notificationIcon.addEventListener("click", function () {
-        closeMenus();
-        notificationPopup.style.display = "block";
-      });
-
-      moreSettingsIcon.addEventListener("click", function () {
-        moreSettingsPopup.style.display = "block";
-      });
-
-      document.addEventListener("click", function (event) {
-        var target = event.target;
-        if (
-          !target.closest(".more-settings-popup") &&
-          !target.closest("#more-settings-icon")
-        ) {
-          moreSettingsPopup.style.display = "none";
-        }
-      });
-
-      function scrollToTop() {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth",
-        });
-      }
-
-      function closeMenus() {
-        searchBar.style.display = "none";
-        notificationPopup.style.display = "none";
-      }
 
 
 
